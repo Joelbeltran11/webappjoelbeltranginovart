61b8a1ee7e043545d75028aa79909df297326566
Joelbeltran11 <joelfrancesc.beltran@institutroquetes.cat>
2025-04-25 13:19:45 +0200

    canvis varios

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let validat = false;    // variable que permet saber si hi ha algun usuari validat
 let nom, contrasenya;
-let scriptURL = "https://script.google.com/macros/s/AKfycbyF-mSLVaX99VAw4NGdvQT76-YC2-CtB1S32kiy3eNY06no29XFOiBDW6QnrdP9j1heDA/exec ";    // s'ha de substituir la cadena de text per la URL del script
-
-// Variables globals
+let scriptURL = "https://docs.google.com/spreadsheets/d/1AT0aq7y_qlLArnA9IYftNafkn8NU2sQXJRAHrRiyq5U/edit?gid=0#gid=0";    // s'ha de substituir la cadena de text per la URL del script
+let model, webcam, prediccions, maxPrediccions;
+let canvas_creat = false;
+let diagrama;
+let valors = [[],[]];
 let usuari;
 let geoID;
 let seccio_origen;
-let storage = localStorage; // Utilitza localStorage
-
-// Funció per canviar de secció
+let storage = localStorage;
+//------------------------------------------------------------------------------------------------------------------------
 function canvia_seccio(num_boto) {
     const menu = document.getElementById("menu");
     const num_botons = menu.children.length;    // el nombre de botons dins de l'element "menu"
@@ -16,14 +17,14 @@ function canvia_seccio(num_boto) {
         let boto = document.getElementById("boto_" + i);
         let seccio = document.getElementById("seccio_" + i);
         if (i == num_boto) {
-            boto.style.color = "#950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
+            boto.style.color = "##950E17";    // es destaca la secció activa amb el canvi de colors del botó corresponent
             boto.style.backgroundColor = "#FCDEE0";
             seccio.style.display = "flex";    // es fa visible la secció activa
         }
         else {
-            boto.style.color = "white";    // colors dels botons de seccions inactives
-            boto.style.backgroundColor = "#950E17";
-            seccio.style.display = "none";    // s'oculten les seccions inactives
+            boto.style.color = "#FCDEE0";    // colors dels botons de seccions inactives
+             boto.style.backgroundColor = "#950E17";
+             seccio.style.display = "none";    // s'oculten les seccions inactives
         }
     }
     if (num_boto == 3) {    // si es prem el botó de la secció "Galeria"
@@ -34,16 +35,12 @@ function canvia_seccio(num_boto) {
         if (typeof geoID === "undefined") {    // si encara no s'han obtingut les dades de localització del dispositiu
             navigator.geolocation.watchPosition(geoExit);    // inicia el seguiment de la localització del dispositiu
         }
+    if (num_boto == 6) {
+        mostra_diagrama();
+         }
     }
 }
-
-// Funció per iniciar la sessió
-function inici_sessio() {
-    validat = true;    // usuari validat
-    document.getElementById("seccio_0").style.display = "none";    // s'oculta la secció de validació d'usuaris
-    canvia_seccio(1);    // es mostra la secció 1
-}
-
+//------------------------------------------------------------------------------------------------------------------------
 // Funció per iniciar la sessió amb les dades del formulari
 function inici_sessio() {
     nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
@@ -63,36 +60,13 @@ function inici_sessio() {
             }
         });
 }
-
-// Funció per crear un nou usuari
-function nou_usuari() {
-    nom = document.getElementById("nom_usuari").value;
-    contrasenya = document.getElementById("contrasenya").value;
-    let consulta_1 = scriptURL + "?query=select&where=usuari&is=" + nom;    // primera consulta per saber si ja existeix algun usuari amb el nom escrit per l'usuari que es vol registrar
-    fetch(consulta_1)
-        .then((resposta) => {
-            return resposta.json();
-        })
-        .then((resposta) => {
-            if(resposta.length == 0) {    // No hi ha cap altre usuari amb el mateix nom
-                let consulta_2 = scriptURL + "?query=insert&values=" + nom + "$$" + contrasenya;    // segona consulta per registrar l'usuari nou
-                fetch(consulta_2)
-                    .then((resposta) => {
-                        if (resposta.ok) {    // s'ha pogut afegir un registre en la base de dades
-                            window.alert("S'ha completat el registre d'usuari.");
-                            inicia_sessio();
-                        }
-                        else {    // no s'ha pogut afegir un registre en la base de dades
-                            alert("S'ha produït un error en el registre d'usuari.");
-                        }
-                    });
-            } 
-            else {    // l'usuari ha de tornar-ho a intentar amb un nom diferent
-                alert("Ja existeix un usuari amb aquest nom.");
-            }
-        });
+//------------------------------------------------------------------------------------------------------------------------
+function inicia_sessio() {
+    validat = true;    // usuari validat
+    document.getElementById("seccio_0").style.display = "none";    // s'oculta la secció de validació d'usuaris
+    canvia_seccio(1);    // es mostra la secció 1
 }
-
+//------------------------------------------------------------------------------------------------------------------------
 // Funció per tancar la sessió
 function tanca_sessio() {
     if (validat) {
@@ -103,8 +77,23 @@ function tanca_sessio() {
     }
 }
 
-// Funció que s'executa quan la pàgina es carrega
+//------------------------------------------------------------------------------------------------------------------------
 window.onload = () => {
+    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
+    }).addTo(mapa);    // s'afegeix la capa al mapa
+    let vegueries = [[41.39, 2.17, "Àmbit metropolità (Barcelona)"],    // llista on cada element és una llista amb els valors de latitud, longitud i nom de vegueria com a elements
+                 [42.17, 0.89, "Alt Pirineu i Aran (Tremp)"],
+                 [41.12, 1.24, "Camp de Tarragona (Tarragona)"],
+                 [41.73, 1.83 ,"Comarques centrals (Manresa)"],
+                 [41.98, 2.82, "Comarques gironines (Girona)"],
+                 [41.62, 0.62, "Ponent (Lleida)"],
+                 [40.81, 0.52, "Terres de l'Ebre (Tortosa)"],
+                 [41.35, 1.70, "Penedès (Vilafranca del Penedès"]];
+for (i in vegueries) {    // per cada element de la llista
+    L.marker([vegueries[i][0], vegueries[i][1]],{title:vegueries[i][2]}).addTo(mapa);
+}
     let base_de_dades = storage.getItem("base_de_dades");
     if(base_de_dades == null) {
         indexedDB.open("Dades").onupgradeneeded = event => {
@@ -129,12 +118,9 @@ window.onload = () => {
         }
     });
 
-    mapa = L.map("seccio_4").setView([41.72, 1.82], 8);    // assigna el mapa a la secció, centrat en el punt i amb el nivell de zoom
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {    // capa d'OpenStreetMap
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'    // autoria de la capa
-    }).addTo(mapa);    // s'afegeix la capa al mapa
 }
 
+//------------------------------------------------------------------------------------------------------------------------
 // Funció per desa la foto a la base de dades
 function desa_foto() {
     let nou_registre = {    // contingut del nou registre de la base de dades
@@ -150,6 +136,7 @@ function desa_foto() {
     };
 }
 
+//------------------------------------------------------------------------------------------------------------------------
 // Funció per mostrar la foto
 function mostra_foto(id) {
     let canvas = document.getElementById("canvas");
@@ -177,6 +164,7 @@ function mostra_foto(id) {
     };
 }
 
+//------------------------------------------------------------------------------------------------------------------------
 // Funció per omplir la galeria de fotos
 function omple_llista() {
     let lista_fotos = document.getElementById("llista_fotos");
@@ -198,13 +186,105 @@ function omple_llista() {
     };
 }
 
+//------------------------------------------------------------------------------------------------------------------------
 // Funció per sortir de la sessió de forma segura
-function geoExit(posicio) {
-    geoID = posicio.coords;    // obtenir dades geogràfiques
-    if (typeof geoID !== "undefined") {
-        console.log(geoID);
+function geoExit(posicio){
+    let latitud = posicio.coords.latitude;
+    let longitud = posicio.coords.longitude;
+    let pixels = 24;    // nombre de píxels de la forma
+    let mida = 2 * pixels;    // mida de visualització en el mapa
+    let ref_vertical = mida / 2;    // distància vertical des del punt superior de la icona fins al punt de la localització
+    let color = "blue";
+    let path = "M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z";    // cadena de text de la forma
+    let cadenaSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ' + pixels + ' ' + pixels + '"><path d="' + path + '" fill="' + color + '" /></svg>';    // construcció de l'element SVG
+    let icona = encodeURI("data:image/svg+xml," + cadenaSVG);    // codificació d'espais i caràcters especials per formar una URL vàlida
+    let icon = L.icon({    // propietats de la icona
+    iconUrl: icona,    // URL de la forma
+    iconSize: [mida, mida],    // mida de la icona
+    iconAnchor: [mida / 2, ref_vertical]    // distàncies (horitzontal i vertical) des del punt superior esquerre de la icona fins al punt de localització
+    }); 
+    if (typeof geoID === "undefined") {    
+        geoID = L.marker([latitud, longitud], {icon:icon, zIndexOffset:100, title:"Usuari"}).addTo(mapa);    // es defineix el marcador  geoID i es situa per sobre dels altres
+    } else {    // primeres dades de localització, es crea el marcador d'usuari 
+        geoID.setLatLng([latitud, longitud]);    // actualització de la posició del marcador d'usuari en el mapa
     }
-    else {
-        alert("No s'ha pogut obtenir la ubicació");
+}
+//------------------------------------------------------------------------------------------------------------------------
+
+async function inicia_video() {
+    const codi_model = "5K46bqkElUP"    // substitueix els asteriscs pel codi del model d'IA que vas crear en una activitat anterior
+    const tmURL = "https://teachablemachine.withgoogle.com/models/" + codi_model;
+    const modelURL = tmURL + "/model.json";
+    const metadataURL = tmURL + "/metadata.json";
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPrediccions = model.getTotalClasses();    // nombre de tipus d'imatges per reconèixer
+    webcam = new tmImage.Webcam(300, 300, true);    // posada en marxa de la webcam
+    await webcam.setup();
+    await webcam.play();
+    window.requestAnimationFrame(loop);    // bucle
+    document.getElementById("icona_video").style.display = "none";    // oculta la icona de la càmera de vídeo
+    document.getElementById("coincidencia").style.display = "flex";    // mostra el text amb la predicció de coincidències
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    prediccions = document.getElementById("prediccions");
+    for (let i = 0; i < maxPrediccions; i++) {
+        prediccions.appendChild(document.createElement("div"));    // es crea un contenidor per a la coincidència de cada tipus d'imatge
     }
+}
+
+//------------------------------------------------------------------------------------------------------------------------
+async function loop() {
+    webcam.update();
+    await prediu();
+    window.requestAnimationFrame(loop);
+}
+
+//------------------------------------------------------------------------------------------------------------------------
+async function prediu() {
+    const prediccio = await model.predict(webcam.canvas);
+    for (let i = 0; i < maxPrediccions; i++) {
+        const classe = prediccio[i].className + ": " + prediccio[i].probability.toFixed(2);    // es conserven dues xifres decimals
+        prediccions.childNodes[i].innerHTML = classe;
+    }
+    
+//------------------------------------------------------------------------------------------------------------------------
+function mostra_diagrama() {
+    if (!canvas_creat) {    // només si no s'ha creat anteriorment
+        diagrama = new Chart(document.getElementById("diagrama"), {
+            type : 'line',    // tipus de diagrama
+            data : {
+                labels : valors[0],    // etiquetes de l'eix X
+                datasets : [
+                        {
+                            data : valors[1],    // valors mesurats
+                            label : "Nivell de llum",    // títol del diagrama
+                            borderColor : "white",    // color de la línia
+                        }]
+            },
+        });
+        peticio();    // funció que sol·licita el valor més recent del canal de ThingSpeak
+        setInterval(peticio, 20000);    // es sol·licita un valor cada 20 segons, un interval de temps adient per a l'entorn ThingSpeak
+        canvas_creat = true;
+    } 
+}
+    
+//------------------------------------------------------------------------------------------------------------------------
+function peticio() {
+    const canal = "2897205";    // s'han de substituir els asteriscs pel codi del canal
+    const camp = "1";    // el camp 1 (nivell de llum)
+    const max_dades = 10;    // nombre de valors que es volen visualitzar simultàniament
+    const ts_url = "https://api.thingspeak.com/channels/" + canal + "/fields/" + camp + "/last.json"    // url que sol·licita el valor més recent
+    fetch(ts_url)
+        .then(resposta => resposta.json())
+        .then(resposta => {
+            let valor = Number(resposta["field1"]);    // converteix el tipus de valor rebut (text) en nombre.
+            document.getElementById("div_valor").innerHTML = valor;
+            if (valors[0].length >= max_dades) {
+                valors[0].shift();    // elimina el primer valor de la llista d'etiquetes
+                valors[1].shift();    // elimina el primer valor de la llista de valors
+            }
+            valors[0].push(new Date().toLocaleTimeString());    // afegeix l'hora actual a la llista d'etiquetes
+            valors[1].push(valor);    // afegeix el valor rebut a la llista de valors
+            diagrama.update();    // actualitza el diagrama d'acord amb el valor rebut
+        });
+}
 }
